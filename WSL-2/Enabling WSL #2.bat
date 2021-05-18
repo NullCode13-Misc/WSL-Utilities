b570861d524a227ca166bfbdc914a769b4417409
NullCode <ishaqahmed.dev@gmail.com>
2021-05-18 14:25:42 +0600

    Update Enabling WSL #2.bat

--- WSL-2/Enabling WSL #2.bat
+++ WSL-2/Enabling WSL #2.bat
@@ -1,14 +1,7 @@
-@echo off
-CLS
-
+@echo off & cls & title Part 2 of Enabling WSL 2 & color 3
 :start
-@title Part 2 of Enabling WSL 2
-@color 3
 powershell -Command "& {.\wsl_update_x64.msi}"
-echo 50 Percent Completed. Press any key to continue: 
-pause>NUL
-powershell -Command "& {wsl --set-default-version 2}"
-cls 
-echo SUCCESS. WSL 2 has been completely installed and setup. Press any key to close.
-pause>NUL
-exit
+echo 50%% completed. Press any key to continue: & pause>NUL
+powershell -Command "& {wsl --set-default-version 2}" 
+cls & echo WSL 2 has been completely installed and set up. & timeout /t 3 >NUL
+exit /b 0
