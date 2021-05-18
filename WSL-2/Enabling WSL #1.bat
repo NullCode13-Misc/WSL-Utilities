a7fbd05ff54ea2b5033a1036f3a08907b1f3e504
NullCode <ishaqahmed.dev@gmail.com>
2021-05-18 14:20:57 +0600

    Code cleanup

--- WSL-2/Enabling WSL #1.bat
+++ WSL-2/Enabling WSL #1.bat
@@ -1,37 +1,23 @@
-@echo off
-CLS
+@echo off & cls
 
-:init
-setlocal DisableDelayedExpansion
-set "batchPath=%~0"
-for %%k in (%0) do set batchName=%%~nk
-set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
-setlocal EnableDelayedExpansion
-
-:checkPrivileges
+:getelevated
+cls & echo Asking for elevated privileges...
 NET FILE 1>NUL 2>NUL
-if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
-
+if '%errorlevel%' == '0' (goto START) else (goto getPrivileges) 
 :getPrivileges
-if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
+if '%1'=='ELEV' ( goto START )
+set "batchPath=%~f0" & set "batchArgs=ELEV" & set "script=%0" & set script=%script:"=%
+IF '%0'=='!script!' ( GOTO PathQuotesDone )
+set "batchPath=""%batchPath%"""
+:PathQuotesDone
+:ArgLoop
+IF '%1'=='' (echo.) else (echo.)
+:EndArgLoop
+ECHO Set UAC = CreateObject^("Shell.Application"^) > "%temp%\OEgetPrivileges.vbs" & ECHO UAC.ShellExecute "cmd", "/c ""!batchPath! !batchArgs!""", "", "runas", 1 >> "%temp%\OEgetPrivileges.vbs"
+"%temp%\OEgetPrivileges.vbs" & del "%temp%\OEgetPrivileges.vbs" 
 
-ECHO Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
-ECHO args = "ELEV " >> "%vbsGetPrivileges%"
-ECHO For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
-ECHO args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
-ECHO Next >> "%vbsGetPrivileges%"
-ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
-"%SystemRoot%\System32\WScript.exe" "%vbsGetPrivileges%" %*
-exit /B
-
-:gotPrivileges
-setlocal & pushd .
-cd /d %~dp0
-if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
-
-:start
-@title Part 1 of Enabling WSL 2
-@color 3
+:START
+cls & title Part 1 of Enabling WSL 2 & color 3
 powershell -Command "& {dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart}" 
 powershell -Command "& {dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart}" 
 powershell -Command "& {shutdown /r /t 15}" 
